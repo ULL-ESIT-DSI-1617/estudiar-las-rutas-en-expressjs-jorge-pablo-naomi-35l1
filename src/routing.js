ae36b121321e059439ada9e0aeab01a8bf4e3e23
alu0100812428 <alu0100812428@ull.edu.es>
2017-03-03 22:42:20 +0000

    # Añadiendo ejemplo de manejadores de rutas #5

--- src/routing.js
+++ src/routing.js
@@ -36,3 +36,27 @@ app.all('/secreto',function(req,res){
 app.get('/in+fo?',function(req,res){
     res.send('La cadena coincide con la expresión regular: /in+fo?');
 });
+
+
+app.get('/ejemplo/a', function (req, res, next) {
+  console.log('La respuesta será enviada por la siguiente función ...');
+  next();
+}, function (req, res) {
+  res.send('Esta es la respuesta.');
+});
+
+var f0 = function (req, res, next) {
+  console.log('Funcion 0');
+  next();
+};
+
+var f1 = function (req, res, next) {
+  console.log('Funcion 1');
+  next();
+};
+
+var f2 = function (req, res) {
+  res.send('Soy la última función.');
+};
+
+app.get('/ejemplo/b', [f0, f1, f2]);

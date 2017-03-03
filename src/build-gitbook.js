7bdd79d5548b9024261b0e630758beb822102186
alu0100812428 <alu0100812428@ull.edu.es>
2017-03-03 23:07:44 +0000

    Añadidos los deploy y modificado el package.json

---
+++ src/build-gitbook.js
@@ -0,0 +1,14 @@
+var gitbook = require('gitbook');
+
+var book = new gitbook.Book('./txt/', {
+
+	config: {
+
+		output: './_book'
+	}
+});
+
+book.parse().then(function(){
+
+	return book.generate("website");
+});
